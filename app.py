b842fee5c711d97eba1d34d5813652f4626f6057
Joseb89 <eldarion1989@gmail.com>
2026-04-21 09:38:33 -0500

    Created REST API for merging PDF files.

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import os
 import shutil
-import pdf
 import config
-
-import tkinter
 
 from pypdf import PdfReader, PdfWriter
 from flask import Flask, request, render_template
@@ -13,64 +10,54 @@ app = Flask(__name__)
 @app.route("/rotate_pdf", methods=["GET", "POST"])
 def rotate_pdf():
     if request.method == "POST":
-        file = request.files["pdf_file"]
+        try:
+            file = request.files["pdf_file"]
 
-        reader = PdfReader(file)
+            reader = PdfReader(file)
 
-        writer = PdfWriter()
+            writer = PdfWriter()
 
-        angle = int(request.form['angle'])
+            angle = int(request.form['angle'])
 
-        for page in reader.pages:
-            page.rotate(angle)
+            for page in reader.pages:
+                page.rotate(angle)
 
-            writer.add_page(page)
+                writer.add_page(page)
 
-        saved_file = os.path.basename(file.filename)
+            saved_file = os.path.basename(file.filename)
 
-        with open(saved_file, "wb") as new_file:
-            writer.write(new_file)   
+            with open(saved_file, "wb") as new_file:
+                writer.write(new_file)   
 
-        shutil.move(saved_file, config.file_path + file.filename)
+            shutil.move(saved_file, config.file_path + file.filename)
+
+        except FileNotFoundError as error:
+            print(error)
+        except PermissionError as error:
+            print(error)    
 
     return render_template("pdf_rotate.html")
 
-@app.route("/merge_pdf", methods=["POST"])
+@app.route("/merge_pdf", methods=["GET", "POST"])
 def merge_pdf():
-    try:
-        root = tkinter.Tk()
-        root.withdraw()
+    if request.method == "POST":
+        try:
+            merger = PdfWriter()
 
-        merger = PdfWriter()
+            files = request.files.getlist("pdf_file")
 
-        file_path1 = pdf.open_pdf_file()
-        file_path2 = pdf.open_pdf_file()
-                
-        merger.append(file_path1)
-        merger.append(file_path2)
+            for file in files:
+                merger.append(file)    
 
-        while True:
-            continue_merge = input("Do you wish to add another file to merge? (Y/N)").capitalize()
+            file_name = request.form["file_name"]        
 
-            if continue_merge == "N":
-                break
+            with open(f"{config.file_path}{"\\"}{file_name}{".pdf"}", "wb") as new_file:
+                merger.write(new_file)
 
-            elif continue_merge == "Y":
-                new_file = pdf.open_pdf_file()
-                merger.append(new_file)
+        except FileNotFoundError:
+            print("File not found.")
 
-            else:
-                print("Please enter a valid response.")
-
-        saved_file_path = os.path.dirname(file_path1)       
-
-        file_name = input("Please input file name: ")         
-
-        with open(f"{saved_file_path}{"\\"}{file_name}{".pdf"}", "wb") as new_file:
-            merger.write(new_file)
-
-    except FileNotFoundError:
-        print("File not found.")
+    return render_template("pdf_merge.html")        
 
 if __name__ == '__main__':
     app.run(debug=True)        
--- pdf.py
+++
@@ -1,7 +0,0 @@
-from tkinter import filedialog
-
-def open_pdf_file():
-    return filedialog.askopenfilename(
-    initialdir="C:\\Users\\acms\\Documents",
-    title='Select File',
-    filetypes=(("PDF Files", "*.pdf"),))
